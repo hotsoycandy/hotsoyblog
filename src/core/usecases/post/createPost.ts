b8b90321e030eee69e8c2631b2dc8afee9d9dbba
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-25 15:36:19 +0900

    implement createPost use case code

--- src/core/usecases/post/createPost.ts
+++ src/core/usecases/post/createPost.ts
@@ -1,6 +1,12 @@
+import R from 'ramda'
+// domain cores
 import { Post } from 'core/domain/post/Post.entity'
 import { PostRepository } from 'core/domain/post/Post.repository'
-import { CommonError } from 'core/common/errors/CommonError'
+// utils
+import { pipeP } from 'core/common/utils'
+// errors
+import type { CommonError } from 'core/common/errors/CommonError'
+import { ValidationError } from 'core/common/errors/ValidationError'
 
 export class CreatePost {
   constructor (
@@ -14,6 +20,27 @@ export class CreatePost {
       authorId: string
     }
   ): Promise<Post | CommonError> {
-    return undefined as any
+    return await pipeP([
+      R.tap(
+        R.pipe(
+          R.prop('title'),
+          validateTitle
+        )
+      ),
+      this.PostRepo.createPost
+    ]
+    )(createParams)
   }
+}
+
+/**
+ * @todo
+ * - generalize this function
+ * - or replace with validation module
+ */
+function validateTitle (title: string): Error | undefined {
+  if (title.length > 50) {
+    return new ValidationError(`"${title}"은 50자를 초과할 수 없습니다.`)
+  }
+  return undefined
 }
