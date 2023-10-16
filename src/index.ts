71a637468c9e208249cffb18d7518f4f1d096bde
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-16 20:23:23 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { startServer } from './server'
-import { connectDB } from './db'
+import { startServer } from './adapters/server'
+import { connectDB } from './infrastructure/db'
 
 async function startApp (): Promise<void> {
   await connectDB()
