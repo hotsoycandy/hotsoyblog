62ea58e1dc74aa7206fa4166d01bf704e8184d62
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-16 20:25:22 +0900

    Update user.entity.ts

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,12 +1,32 @@
+import crypto from 'crypto'
+import { z as zod } from 'zod'
+import { Entity, PrimaryColumn, Column } from 'typeorm'
 import { generateID } from 'common/utils/generateID'
+import { ValidationError } from 'common/errors/ValidationError'
 
+@Entity()
 export class User {
-  private readonly _idx: string
-  private _email: string
-  private _password: string
-  private _nickname: string
-  private readonly _createdAt: Date
-  private _updatedAt: Date
+  @PrimaryColumn()
+  public readonly idx: string
+
+  @Column()
+  public email: string
+
+  @Column()
+  public password: string
+
+  @Column()
+  public nickname: string
+
+  @Column({
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  public readonly createdAt: Date
+
+  @Column({
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  public updatedAt: Date
 
   constructor (createParam: {
     idx?: string
@@ -16,57 +36,73 @@ export class User {
     createdAt?: Date
     updatedAt?: Date
   }) {
-    this._idx = createParam.idx ?? generateID('USER')
-    this._email = createParam.email
-    this._password = createParam.password
-    this._nickname = createParam.nickname
-    this._createdAt = createParam.createdAt ?? new Date()
-    this._updatedAt = createParam.updatedAt ?? new Date()
+    this.idx = createParam?.idx ?? generateID('USER')
+    this.email = createParam?.email
+    this.password = createParam?.password
+    this.nickname = createParam?.nickname
+    this.createdAt = createParam?.createdAt ?? new Date()
+    this.updatedAt = createParam?.updatedAt ?? new Date()
   }
 
-  // idx
-  get idx (): string {
-    return this._idx
+  static async signup (
+    userParams: {
+      email: string
+      password: string
+      nickname: string
+    },
+    utilParams: {
+      isEmailDuplicated: (email: string) => Promise<boolean>
+    }
+  ): Promise<User> {
+    // validation
+    const validator = zod
+      .object({
+        email: zod
+          .string()
+          .email('이메일 형식이 올바르지 않습니다.'),
+        password: zod
+          .string()
+          .regex(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,50}$/,
+            '비밀번호는 최소 8자리에 영문, 숫자, 특수문자를 하나 이상 포함하고 있어야 합니다.'),
+        nickname: zod
+          .string()
+          .regex(
+            /^[a-zA-Z0-9ㄱ-하ㅏ-ㅣ가-힣_-]{2,20}$/,
+            '별명은 영문, 한글, 숫자, _(언더바), -(대시)만 사용할 수 있으며 최소 2자 최대 20자입니다.')
+      })
+      .strict()
+
+    const validationResult = validator.safeParse(userParams)
+    if (!validationResult.success) {
+      const validationErrorMessage = validationResult
+        .error
+        .errors
+        .map(err => `"${err.path.join('.')}": ${err.message}`)
+        .join('. ')
+      throw new ValidationError(validationErrorMessage)
+    }
+
+    const { email, password, nickname } = userParams
+    const { isEmailDuplicated } = utilParams
+
+    // email duplication check
+    if (await isEmailDuplicated(email)) {
+      throw new ValidationError('이메일이 중복되었습니다.')
+    }
+
+    // return new user
+    return new User({
+      email: email.toLowerCase(),
+      password: User.createHashedPassword(password),
+      nickname
+    })
   }
 
-  // id
-  get email (): string {
-    return this._email
-  }
-
-  set email (newEmail: string) {
-    this._email = newEmail
-  }
-
-  // password
-  get password (): string {
-    return this._password
-  }
-
-  set password (newPassword: string) {
-    this._password = newPassword
-  }
-
-  // nickname
-  get nickname (): string {
-    return this._nickname
-  }
-
-  set nickname (newNickname: string) {
-    this._nickname = newNickname
-  }
-
-  // createdAt
-  get createdAt (): Date {
-    return this._createdAt
-  }
-
-  // updatedAt
-  get updatedAt (): Date {
-    return this._updatedAt
-  }
-
-  set updatedAt (newUpdatedAt: Date) {
-    this._updatedAt = newUpdatedAt
+  static createHashedPassword (password: string): string {
+    return crypto
+      .createHash('sha512')
+      .update(password)
+      .digest('base64')
   }
 }
