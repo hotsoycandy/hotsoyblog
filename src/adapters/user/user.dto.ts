295950ffc7070f45aeb9d6214370a8ce0a57aca1
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-18 23:39:44 +0900

    user.dto.ts 생성
    회원가입,로그인 등 유저 관련 API 에서 중요한 정보들을 가려내기 위해 dto를 생성

--- src/adapters/user/user.dto.ts
+++ src/adapters/user/user.dto.ts
@@ -2,4 +2,18 @@ export class UserDTO {
   public idx: string
   public email: string
   public nickname: string
+
+  constructor (userData: { idx: string, email: string, nickname: string }) {
+    this.idx = userData.idx
+    this.email = userData.email
+    this.nickname = userData.nickname
+  }
+
+  toJSON (): { idx: string, email: string, nickname: string } {
+    return {
+      idx: this.idx,
+      email: this.email,
+      nickname: this.nickname
+    }
+  }
 }
