125a0e58f860a21aff83589c213189c717dfa7bc
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-20 21:12:59 +0900

    user.dto - createdAt, updatedAt 필드 추가

--- src/adapters/user/user.dto.ts
+++ src/adapters/user/user.dto.ts
@@ -1,19 +1,41 @@
+interface UserDTOJSON {
+  idx: string
+  email: string
+  nickname: string
+  createdAt: string
+  updatedAt: string
+}
+
 export class UserDTO {
   public idx: string
   public email: string
   public nickname: string
+  public createdAt: Date
+  public updatedAt: Date
 
-  constructor (userData: { idx: string, email: string, nickname: string }) {
-    this.idx = userData.idx
-    this.email = userData.email
-    this.nickname = userData.nickname
+  constructor (
+    userDataParams: {
+      idx: string
+      email: string
+      nickname: string
+      createdAt: Date
+      updatedAt: Date
+    }
+  ) {
+    this.idx = userDataParams.idx
+    this.email = userDataParams.email
+    this.nickname = userDataParams.nickname
+    this.createdAt = userDataParams.createdAt
+    this.updatedAt = userDataParams.updatedAt
   }
 
-  toJSON (): { idx: string, email: string, nickname: string } {
+  toJSON (): UserDTOJSON {
     return {
       idx: this.idx,
       email: this.email,
-      nickname: this.nickname
+      nickname: this.nickname,
+      createdAt: this.createdAt.toISOString(),
+      updatedAt: this.updatedAt.toISOString()
     }
   }
 }
