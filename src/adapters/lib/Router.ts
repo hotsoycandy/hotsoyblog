52c8baa8be1e817b648d0b9cb1268286e5e56877
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-20 21:11:16 +0900

    Router class - addValidation 코드 body뿐 아니라 query, params도 검사하도록 변경

--- src/adapters/lib/Router.ts
+++ src/adapters/lib/Router.ts
@@ -1,7 +1,7 @@
 import R from 'ramda'
-import passport from 'passport'
 import { RequestHandler, Router as ExpressRouter } from 'express'
-import { AnyZodObject } from 'zod'
+import passport from 'passport'
+import { z as zod, AnyZodObject } from 'zod'
 import { ValidationError } from 'common/errors/ValidationError'
 // import { UnauthorizedError } from 'common/errors/UnauthorizedError'
 
@@ -20,29 +20,68 @@ export default class Router {
     return this
   }
 
-  addValidation (validator: { body: AnyZodObject }): Router {
+  addValidation (
+    {
+      body: bodyValidator = zod
+        .object({})
+        .strict(),
+      query: queryValidator = zod
+        .object({})
+        .strict(),
+      params: paramsValidator = zod
+        .object({})
+        .strict()
+    }: {
+      body?: AnyZodObject
+      query?: AnyZodObject
+      params?: AnyZodObject
+    }
+  ): Router {
+    type ValidateTargetField = 'body' | 'query' | 'params'
+
+    const validator = {
+      body: bodyValidator,
+      query: queryValidator,
+      params: paramsValidator
+    }
+
     this.handlers.push(
       (req, res, next) => {
-        const validationResult = validator.body.safeParse(req.body)
-        if (validationResult.success) return next()
-
-        const validationErrorMessage: string = R.pipe(
-          R.pick(['formErrors', 'fieldErrors']),
-          R.evolve({
-            formErrors: R.identity,
-            fieldErrors: R.pipe(
-              R.toPairs,
-              R.map(([key, values = []]) => `"${key}: ${(values as string[]).join(', ')}`),
-              R.join('. ')
-            )
+        R.pipe(
+          R.always(req),
+          R.pickAll(['body', 'query', 'params']),
+          R.mapObjIndexed((value: object, field: ValidateTargetField) => {
+            const validationResult = validator[field].safeParse(value)
+            return validationResult.success
+              ? {}
+              : validationResult.error.flatten()
+          }),
+          R.pickBy(R.complement(R.equals({}))),
+          R.mapObjIndexed((value: object, field: ValidateTargetField) => {
+            return R.pipe(
+              R.always(value),
+              R.pick(['formErrors', 'fieldErrors']),
+              R.evolve({
+                formErrors: R.map((message: string) => `"${field}": ${message}`),
+                fieldErrors: R.pipe(
+                  R.toPairs,
+                  R.map(([key, values = []]) => `"${field}.${key}": ${(values as string[]).join(', ')}`)
+                )
+              }),
+              R.values
+            )()
           }),
           R.values,
-          R.join('. ')
-        )(validationResult.error.flatten())
-
-        next(new ValidationError(validationErrorMessage))
-      }
-    )
+          R.flatten,
+          R.join(', '),
+          R.ifElse(
+            R.equals(''),
+            R.always(null),
+            (messages: string) => new ValidationError(messages)
+          ),
+          next
+        )()
+      })
 
     return this
   }
