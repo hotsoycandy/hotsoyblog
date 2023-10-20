d6b8fec6d0ec57e33dd4580cf51de82b68eb218c
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-20 15:10:45 +0900

    post.controller/move middleware code to Router class

--- src/adapters/post/post.controller.ts
+++ src/adapters/post/post.controller.ts
@@ -1,47 +1,15 @@
 import { z as zod } from 'zod'
-import { Router, RequestHandler } from 'express'
-import { CreatePost } from 'application/usecases/posts/createPost'
+import { Router as ExpressRouter, RequestHandler } from 'express'
+import { CreatePost } from 'application/post/usecases/createPost.usecase'
 import { PostRepositoryImp } from 'infrastructure/post/post.repositoryImp'
 import { ValidationError } from 'common/errors/ValidationError'
 
-const router = Router()
+const router = ExpressRouter()
 const createPostInstance = new CreatePost(new PostRepositoryImp())
 
-const a: RequestHandler = (req, res, next) => {
-  const validator =
-    zod
-      .object({
-        authorId: zod.string(),
-        title: zod.string(),
-        content: zod.string()
-      })
-      .strict()
+const createPostRouter
 
-  const validationResult = validator.safeParse(req.body)
-  if (validationResult.success) return next()
-
-  const validationErrorMessage =
-    validationResult
-      .error
-      .errors
-      .map(err => `"${err.path.join('.')}": ${err.message}`)
-      .join('. ')
-  next(new ValidationError(validationErrorMessage))
-}
-
-const b: RequestHandler = (req, res, next) => {
-  createPostInstance.execute(req.body)
-    .then(result => {
-      return res.json(result)
-    })
-    .catch(err => {
-      next(err)
-    })
-}
-
-router.get('/', [a, b])
-
-const postRouter = Router()
+const postRouter = ExpressRouter()
 postRouter.use('/posts', router)
 
 export default postRouter
