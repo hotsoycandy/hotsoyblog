015fa2f2044b501c9277741ccb1c1f3b7362acc4
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-20 21:13:38 +0900

    post.controller - 모든 라우터 삭제
    새롭게 추가된 Router Class를 적용하기 위해 일단 기존 코드 모두 삭제

--- src/adapters/post/post.controller.ts
+++ src/adapters/post/post.controller.ts
@@ -1,15 +1,5 @@
-import { z as zod } from 'zod'
-import { Router as ExpressRouter, RequestHandler } from 'express'
-import { CreatePost } from 'application/post/usecases/createPost.usecase'
-import { PostRepositoryImp } from 'infrastructure/post/post.repositoryImp'
-import { ValidationError } from 'common/errors/ValidationError'
-
-const router = ExpressRouter()
-const createPostInstance = new CreatePost(new PostRepositoryImp())
-
-const createPostRouter
+import { Router as ExpressRouter } from 'express'
 
 const postRouter = ExpressRouter()
-postRouter.use('/posts', router)
 
 export default postRouter
