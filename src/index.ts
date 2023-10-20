5c68eb0d8c6d663852abf826d32ff913ace49f73
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-20 18:14:17 +0900

    add dotenv for hide sensitive informations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv'
 import { startServer } from './adapters/server'
 import { connectDB } from './infrastructure/db'
 
 async function startApp (): Promise<void> {
+  dotenv.config({ path: process.env['ENV_PATH'] })
   await connectDB()
   await startServer()
 }
--- src/infrastructure/db.ts
+++ src/infrastructure/db.ts
@@ -5,11 +5,11 @@ import { Post } from 'domain/post/post.entity'
 
 export const AppDataSource = new DataSource({
   type: 'mariadb',
-  host: 'localhost',
-  port: 3306,
-  username: 'test',
-  password: 'ZWADrGj3gawzlTq',
-  database: 'hotsoyblog',
+  host: process.env['DB_HOST'],
+  port: parseInt(process.env['DB_PORT'] ?? '3306'),
+  username: process.env['DB_USER'],
+  password: process.env['DB_PASSWORD'],
+  database: process.env['DB_NAME'],
   synchronize: true,
   logging: process.env['NODE_ENV'] === 'development',
   entities: [User, Post],
