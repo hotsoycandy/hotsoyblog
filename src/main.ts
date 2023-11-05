c477b2c1b8fcfc5db454d8413b58f530e86a20a6
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-05 14:56:29 +0900

    Style: Format codes for new linter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
+  .catch(err => {
+    console.error('Unexpected error caught while running application', err)
+  })
