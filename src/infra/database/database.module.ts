f7cb456be6d202c8b2dd3823770632b20abb3a29
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-15 15:39:00 +0900

    Feat: [database.module] Create module

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Logger, Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Card } from 'src/card/entities/card.entity'
+import { NestjsTypeOrmLogger } from './nestjs-typeorm-logger'
+
+function isDevEnv(NODE_ENV: string): boolean {
+  return ['development', 'test'].includes(NODE_ENV)
+}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get('DATABASE_HOST'),
+        port: parseInt(config.get('DATABASE_PORT') ?? '3306'),
+        username: config.get('DATABASE_USERNAME'),
+        password: config.get('DATABASE_PASSWORD'),
+        database: config.get('DATABASE_NAME'),
+        logger: new NestjsTypeOrmLogger(new Logger('TypeORM')),
+        entities: [Card],
+        synchronize: isDevEnv(config.get('NODE_ENV') as string),
+        logging: isDevEnv(config.get('NODE_ENV') as string),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
