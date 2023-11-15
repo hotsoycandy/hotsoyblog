f948123e42f4a8696ef439fbadd18e08394684ae
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-15 15:27:05 +0900

    Feat: [set-nest-app] Create & Apply lib

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { setNestApp } from './infra/server/set-nest-app'
 
-async function bootstrap (): Promise<void> {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+  setNestApp(app)
   await app.listen(3000)
 }
-bootstrap()
-  .catch(err => {
-    console.error('Unexpected error caught while running application', err)
-  })
+
+bootstrap().catch((err) => {
+  console.error('Unexpected error caught while running application', err)
+})
