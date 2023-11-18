24aa2e3bb22ef48e1b9bbb164c36f29a2c8f5c4e
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-18 22:21:50 +0900

    Feat: [card.dto] Create

---
+++ src/card/dto/card.dto.ts
@@ -0,0 +1,20 @@
+import { Expose } from 'class-transformer'
+import { Card } from '../entity/card.entity'
+import { pick } from 'lodash'
+
+@Expose({ toPlainOnly: true })
+export class CardDto {
+  public originURL!: string
+  public title!: string
+  public description!: string
+  public favicon?: string
+
+  static from(card: Card): CardDto {
+    const cardDto = new CardDto()
+    Object.assign(
+      cardDto,
+      pick(card, ['originURL', 'title', 'description', 'favicon']),
+    )
+    return cardDto
+  }
+}
