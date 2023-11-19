edfd4a728b1e7ee2ac1c8e6259722c331950951c
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-19 20:43:32 +0900

    Feat: [index.html] Create & Serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { DatabaseModule } from './infra/database/database.module'
 import { CardModule } from './card/card.module'
 
 @Module({
-  imports: [DatabaseModule, CardModule],
+  imports: [
+    DatabaseModule,
+    CardModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
