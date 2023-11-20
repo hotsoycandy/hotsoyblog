287af7ae56cf7e9795dd64addfc98e9e606e521e
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-20 23:16:16 +0900

    File: Create auth module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { DatabaseModule } from './infra/database/database.module'
 import { CardModule } from './card/card.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     DatabaseModule,
-    CardModule,
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'public'),
     }),
+    CardModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
