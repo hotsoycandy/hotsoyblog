26e1ef2ffa2b81f91894343e28038ed43b8d2f59
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-21 00:33:17 +0900

    Feat: [user.entity] Create entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,13 @@
+export class User {
+  public idx!: string
+
+  public email!: string
+
+  public password!: string
+
+  public nickname!: string
+
+  public createdAt!: Date
+
+  public updatedAt!: Date
+}
