f2825b72aacaedccaa18ecc114e7fff74aaf8915
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-21 22:13:13 +0900

    Feat: [user.entity] Add newUser

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,9 +1,11 @@
 import { User } from './entity/user.entity'
 
 export abstract class UserRepository {
-  abstract createUser(createUserParams: {
+  abstract newUser(createUserParams: {
     email: string
     password: string
     nickname: string
   }): Promise<User>
+
+  abstract createUser(user: User): Promise<User>
 }
