b283a7db90d765d6e0eb12c9061cee6dab67e759
hotsoycandy <wkdwnsghd617@gmail.com>
2023-11-27 10:05:44 +0900

    Feat: [user.service] Add createUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { User } from './entity/user.entity'
+import { UserRepository } from './user.repository'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(public readonly userRepository: UserRepository) {}
+
+  async createUser(createUserParams: {
+    email: string
+    password: string
+    nickname: string
+  }): Promise<User> {
+    throw new Error('Method is not ready yet')
+  }
+}
